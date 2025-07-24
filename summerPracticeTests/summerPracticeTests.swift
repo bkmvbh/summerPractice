51f66ea8feaa562ecee3db034192d08a25ef77d4
bkmvbh <112980038+bkmvbh@users.noreply.github.com>
2025-07-24 18:59:23 +0300

    Initial Commit

---
+++ summerPracticeTests/summerPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  summerPracticeTests.swift
+//  summerPracticeTests
+//
+//  Created by Ильмир Шарафутдинов on 24.07.2025.
+//
+
+import Testing
+@testable import summerPractice
+
+struct summerPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
